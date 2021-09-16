3e1b298db6504c82eb0e7e6e0350ffd28a74d296
AdrianPin <adria@DESKTOP-AVIA2KN>
2021-09-16 12:24:26 -0400

    Agregando frase motivadora

--- Ventas1/Ventas1/Form1.cs
+++ Ventas1/Ventas1/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("El futuro pertenece a los que nunca dejan de soñar");
         }
     }
 }
