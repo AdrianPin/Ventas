33e3b0cc8726d86c99d13d93ede2ba7c3c551f02
AdrianPin <90521131+AdrianPin@users.noreply.github.com>
2021-09-21 13:59:58 -0400

    Add files via upload

---
+++ Ventas1/SistemaRentas/Rentas/Win.Rentas/FormMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void preciosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void sushiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formLogin = new FormLogin();
+            formLogin.ShowDialog();
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formMenu = new FormMenu();
+            formMenu.MdiParent= this;
+            formMenu.Show();
+        }
+    }
+}
